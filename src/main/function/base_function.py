178e8a4fb10961acfa0daed34f40e7d144a024cd
Anton Riabushev <ryabushev96@gmail.com>
2020-02-13 17:10:26 +0300

    add loss for regression and linear functions

--- src/main/function/base_function.py
+++ src/main/function/base_function.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class BaseFunction:
@@ -10,3 +11,8 @@ class BaseFunction:
 
     def guassian(self):
         raise RuntimeError("Should be overridden")
+
+    def _regularization_part(self, w, l1, l2):
+        l1_part = np.abs(w).mean()
+        l2_part = (w ** 2).mean()
+        return l1 * l1_part + l2 / 2 * l2_part
--- src/main/function/linear.py
+++ src/main/function/linear.py
@@ -3,4 +3,10 @@ from src.main.function.base_function import BaseFunction
 
 
 class Linear(BaseFunction):
-    pass
+
+    # MOCK. Should be checked all shapes
+    def _loss(self, X, y, w, l1, l2):
+        diff = y - X.dot(w)  # Size (num_of_samples, 1)
+        main = (diff ** 2).mean()
+        total = main + self._regularization_part(w, l1, l2)
+        return total
