bug fix in generate poisson regression. add linear search to gradient descent

src/generate_dataset/dataset_processing.py
@@ -33,7 +33,7 @@ def generate_dataset_poisson_regression(args):
     X = generate_X(args)  # Size: (num_samples, num_features)
     w = np.random.rand(args.number_features, 1)
     mean_y = X.dot(w)
-    y = np.random.poisson(lam=mean_y)  # Size: (number_samples, 1)
+    y = np.random.poisson(lam=np.exp(mean_y))  # Size: (number_samples, 1)
     return X, y, w
 
 

src/main/main.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 from src.generate_dataset.dataset_processing import load_dataset
@@ -12,6 +14,7 @@ if __name__ == "__main__":
     args = argument_parser.parse_args()
     if not args.no_use_save_args_settings:
         load_args_settings(args)
+    random.seed(args.seed)
     np.random.seed(args.seed)
 
     X, y, w = load_dataset(args)
@@ -19,9 +22,11 @@ if __name__ == "__main__":
     y = y.reshape((S, 1))
     assert y.shape == (S, 1)
     assert w.shape == (F, 1)
+
+    function = get_function(args)
+    stop_condition = get_stop_condition(args, F)
+
     with SummaryWriter(log_dir=f'runs/{args.function_name}-{args.optim_method}', purge_step=0) as writer:
-        function = get_function(args)
-        stop_condition = get_stop_condition(args, F)
         opt_method_maker = get_opt_method_maker(args, writer)
         opt_method = opt_method_maker(X, y.reshape((-1, 1)), function, stop_condition)
         answer = opt_method.run()

src/main/optim_methods/gradientdescent.py
@@ -4,6 +4,7 @@ import scipy.optimize
 from src.main.functions.interface_function import InterfaceFunction
 from src.main.optim_methods.interface_method_optim import InterfaceMethodOptim, InterfaceOptimAnswer
 from src.main.stop_conditions.common import InterfaceStopCondition
+import numpy as np
 
 
 class GradientDescent(InterfaceMethodOptim):
@@ -14,12 +15,16 @@ class GradientDescent(InterfaceMethodOptim):
     def step(self):
         grad = self._function.loss_gradient(self._w, self._X, self._y)
         assert grad.shape == self._w.shape
-        optim_func = lambda dw: self._function.loss(self._w + dw * grad, self._X, self._y)
+        optim_func = lambda dw: self._function.loss(self._w - dw * grad, self._X, self._y)
         # May be there bounds write like BAD SMELL
         res = scipy.optimize.minimize_scalar(optim_func, method="bounded", bounds=(0, 1))
         lr = res['x']
-        self._tensorboard_writer.add_scalar('lr', lr, self._learning_step)
         self._w -= lr * grad
+
+        # Tensorboard part
+        self._tensorboard_writer.add_scalar('lr', lr, self._learning_step)
+        loss = self._function.loss(self._w, self._X, self._y)
+        self._tensorboard_writer.add_scalar('loss', loss, self._learning_step)
 
     def get_answer(self):
         return InterfaceOptimAnswer(self._w_start, self._w, self._function)