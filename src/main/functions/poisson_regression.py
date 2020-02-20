fix bug with hessian. add common code for linear search. add linear search to newton. newton and poisson regression is working now.

src/main/functions/poisson_regression.py
@@ -39,6 +39,6 @@ class PoissonRegression(InterfaceFunction):
         exp_part = np.exp(xw)
         # MOCK should be checked
         M = scipy.sparse.diags([exp_part.view().reshape(-1)], [0]) # (S, S)
-        main = X.T.dot(M).dot(X)
+        main = -X.T.dot(M).dot(X)
         total = self._maximization_to_minimization * main + self._loss_hessian_regularization_part(w)
         return total

src/main/optim_methods/gradientdescent.py
@@ -20,11 +20,8 @@ class GradientDescent(InterfaceMethodOptim):
         res = scipy.optimize.minimize_scalar(optim_func, method="bounded", bounds=(0, 1))
         lr = res['x']
         self._w -= lr * grad
+        self._tensorboard_part(lr)
 
-        # Tensorboard part
-        self._tensorboard_writer.add_scalar('lr', lr, self._learning_step)
-        loss = self._function.loss(self._w, self._X, self._y)
-        self._tensorboard_writer.add_scalar('loss', loss, self._learning_step)
 
     def get_answer(self):
         return InterfaceOptimAnswer(self._w_start, self._w, self._function)

src/main/optim_methods/newton.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from src.main.optim_methods.interface_method_optim import InterfaceMethodOptim, InterfaceOptimAnswer
+from src.main.optim_methods.utils import get_lr
 
 
 class Newton(InterfaceMethodOptim):
@@ -15,7 +16,11 @@ class Newton(InterfaceMethodOptim):
         # и уметь обрабатывать наличие неположительных собственных чисел
         # в гессиане) MOCK. NOT DONE
         coef = np.linalg.inv(hessian)
-        self._w -= coef.dot(grad)
+        direction = coef.dot(grad)
+        # BAD SMELL, max_bound like magic const
+        lr = get_lr(self._w, direction, self._X, self._y, self._function, max_bound=1000)
+        self._w -= lr * direction
+        self._tensorboard_part(lr)
 
     def get_answer(self):
         return InterfaceOptimAnswer(self._w_start, self._w, self._function)