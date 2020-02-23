680694125c17427eaab0020d845cde91f3192836
Anton Riabushev <ryabushev96@gmail.com>
2020-02-23 14:45:52 +0300

    fix bugs in hfn

--- src/main/optim_methods/hessian_free_newton.py
+++ src/main/optim_methods/hessian_free_newton.py
@@ -11,14 +11,13 @@ class HessianFreeNewton(InterfaceMethodOptim):
         S, F = self._X.shape
         grad = self._function.loss_gradient(self._w, self._X, self._y)
         assert grad.shape == (F, 1)
-        hessian = self._function.loss_hessian(self._w, self._X, self._y)
-        assert hessian.shape == (F, F)
         assert self._w.shape == (F, 1)
 
-        direction = self._direction(grad, hessian, self._w)
+        direction = self._direction(grad, self._w)
 
         # BAD SMELL, max_bound like magic const
-        lr = get_lr(self._w, direction, self._X, self._y, self._function, max_bound=1000)
+        max_bound = 1
+        lr = get_lr(self._w, direction, self._X, self._y, self._function, max_bound=max_bound)
         self._w -= lr * direction
 
         self._tensorboard_part(lr)
@@ -30,33 +29,36 @@ class HessianFreeNewton(InterfaceMethodOptim):
     def _get_stop_condition(self, F) -> InterfaceStopCondition:
         return NumIterStopCondition(F)
 
-    def _direction(self, grad, hessian, w_start):
+    def _direction(self, grad, w_start):
         """
-        solve Ax = b
         https://habr.com/ru/post/350794/
-
-        :param grad:
-        :param hessian:
-        :param w_start:
-        :return:
+        http://andrew.gibiansky.com/blog/machine-learning/hessian-free-optimization/
         """
         F = grad.shape[0]
         assert grad.shape == (F, 1)
-        assert hessian.shape == (F, F)
         assert w_start.shape == (F, 1)
-        A = hessian.copy()
+
+        def A_dot_vec(x):
+            # BAD SMELL
+            eps = 1e-6
+            new_w = w_start + eps * x
+            new_grad = self._function.loss_gradient(new_w, self._X, self._y)
+            res = (new_grad - grad) / eps
+            assert not np.isnan(res).any()
+            return res
+
         x = np.random.rand(F, 1)
         b = grad.copy()
-        r = b - A.dot(x)  # residual
+        r = b - A_dot_vec(x)  # residual
         d = r  # search direction
         stop_condition = self._get_stop_condition(F=F)
         while not stop_condition.finish(x):
 
-            divider1 = d.T.dot(A).dot(d)
+            divider1 = d.T.dot(A_dot_vec(d))
             alpha = r.T.dot(r) / (divider1 + self._eps_for_zero_division) # (1, 1)
 
             x_next = x + d.dot(alpha)
-            r_next = r - A.dot(d).dot(alpha)
+            r_next = r - A_dot_vec(d).dot(alpha)
 
             divider2 = r.T.dot(r)
             beta = r_next.T.dot(r_next) / (divider2 + self._eps_for_zero_division)
@@ -67,4 +69,6 @@ class HessianFreeNewton(InterfaceMethodOptim):
             r = r_next
             d = d_next
 
-        return x
+        w_next = x
+
+        return w_next
